718775d43ec942571bf963f0da95a43514c2d2f9
bassem-krayem <bassem.krayem09@gmail.com>
2026-04-16 15:30:20 +0100

    feature: changing the extention from js to mjs

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,12 +1,12 @@
 // change to import instaid
-import { createClient } from "redis";
-import { promisify } from "util";
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
     this.client = createClient();
     this.getAsync = promisify(this.client.get).bind(this.client);
-    this.client.on("error", (error) => {
+    this.client.on('error', (error) => {
       console.log(`Redis client not connected to the server: ${error}`);
     });
   }
